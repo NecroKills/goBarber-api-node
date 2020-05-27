dea2a556ae5d1b6377e37b6e80f9a18ca0eb33fd
Maycon <maycondasilvamoreira@hotmail.com>
2020-05-27 20:16:01 -0300

    Foi Desenvolvido o provider de envio de email usando o ethereal e foi registrado o provider de email no nosso dependency inject container.

--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -2,7 +2,16 @@ import { container } from 'tsyringe';
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
 
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import EherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+// Foi feito diferente pois com o Singleton naõ estava entrando no constructor
+container.registerInstance<IMailProvider>(
+  'EherealMailProvider',
+  new EherealMailProvider(),
 );
