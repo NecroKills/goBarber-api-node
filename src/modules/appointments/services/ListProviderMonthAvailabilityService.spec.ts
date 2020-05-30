7d4ab16eb1a66caea6a53fb97e520963be8026df
Maycon <maycondasilvamoreira@hotmail.com>
2020-05-30 13:56:45 -0300

    Foi desenvolvido a funcionalidade onde o usuario deve poder listar os dias de um mes com pelo menos um horario disponivel de um prestador. Foi criado um novo service para listar a disponibilidade do prestador de serviço no mes. Tambem foi criado o seu arquivo de testes onde possui o teste que deve poder listar a disponibilidade do mês do provedor.

---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -0,0 +1,54 @@
+import 'reflect-metadata';
+import ListProviderMonthAvailabilityService from '@modules/appointments/services/ListProviderMonthAvailabilityService';
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+
+let listProviderMonthAvailability: ListProviderMonthAvailabilityService;
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+
+// describe: significa categoria ou seja Testes da categoria CreateAppointment
+// it deixa mais claro o teste que estamos fazendo.
+describe('ListProviderMonthAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderMonthAvailability = new ListProviderMonthAvailabilityService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the month availability from provider', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 3, 20, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 10, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 8, 0, 0),
+    });
+
+    const availability = await listProviderMonthAvailability.execute({
+      provider_id: 'user',
+      year: 2020,
+      month: 5,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { day: 19, availability: true },
+        { day: 20, availability: false },
+        { day: 21, availability: false },
+        { day: 22, availability: true },
+      ]),
+    );
+  });
+});
