6e3b9328efe8d93e05fcba77e195d07fc289a164
Maycon <maycondasilvamoreira@hotmail.com>
2020-05-30 14:30:12 -0300

    Foi feito para listar todos os dias do mês e para mostrar se nesse dia esta disponivel ou não. Foi desenvolvido para saber quantos dias tem no mes  usando o getDaysInMonth do date-fns, este retorna o numero de dias do mes. Tambem foi modificado o teste para preencher todos os horarios com agendamentos no dia 20

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
@@ -33,14 +34,28 @@ class ListProviderMonthAvailabilityService {
       },
     );
 
-    console.log(appointments);
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-    return [
-      {
-        day: 1,
-        available: false,
-      },
-    ];
+    // Cria um array com o tamanho de dias do mes
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      // Compara menor que 10 pois existem 10 agendamentos
+      // no dia das 8 até as 17 levando em conta 1 hora cada agendamento
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
