21142ec8dc441a27c32b26d8409a0a73649eec90
Maycon <maycondasilvamoreira@hotmail.com>
2020-05-30 22:40:20 -0300

    Foi adicionado as rotas e os controllers da disponibilidade do prestador. Foi criado dois controllers o ProviderDayAvailabilityController e o ProviderMonthAvailabilityController. Tambem foi criado duas rotas já no arquivo de rotas existente providers.routes, para verificar a disponibilidade no mes e tambem a disponibilidade do dia.

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,28 @@
 import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const provideDayAvailabilityController = new ProviderDayAvailabilityController();
+const provideMonthAvailabilityController = new ProviderMonthAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+
+providersRouter.get(
+  '/:provider_id/month-availability',
+  provideMonthAvailabilityController.index,
+);
+
+providersRouter.get(
+  '/:provider_id/day-availability',
+  provideDayAvailabilityController.index,
+);
 
 export default providersRouter;
