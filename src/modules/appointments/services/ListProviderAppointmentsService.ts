3f0ef84bf9864e30117a630118576d3e20b1cf41
Maycon <maycondasilvamoreira@hotmail.com>
2020-06-04 20:51:37 -0300

    Foi feito a configuração inicial do cache, utilizamos Redis para salvar o cache da nossa aplicação. Criamos o arquivo RedisCacheProviders, nele começamos a codificar o save e o recover. Criamos o model ICacheProvider onde ele possui 3 metodos que serão o save, recover e o invalidate. Tambem fizemos uma modificalção no arquivo listProviderAppointmentsService para verificar se o Redis estava configurado certinho.

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   provider_id: string;
@@ -15,6 +16,9 @@ class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -23,12 +27,18 @@ class ListProviderAppointmentsService {
     month,
     year,
   }: IRequest): Promise<Appointment[]> {
+    const cacheData = await this.cacheProvider.recover('asd');
+
+    console.log(cacheData);
+
     const appointments = this.appointmentsRepository.findAllInDayFromProvider({
       provider_id,
       day,
       month,
       year,
     });
+
+    // await this.cacheProvider.save('asd', 'asd');
 
     return appointments;
   }
