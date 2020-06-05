372ec1d5f5f01681ff0c70a37bb6ba35622eb6cf
Maycon <maycondasilvamoreira@hotmail.com>
2020-06-04 23:29:59 -0300

    Foi instalado o pacote rate-limiter-flexible e foi feito sua configuração, ele serve Para prevenir a aplicação de ataques de brute force,  ou para conseguir a senha de alguém ou para evitar ataques ddos. Foi criado um novo middlewares chaamdo rateLimiter, que limita a quantidade de requisição que o usuario faz por segundo. Precisamos adicionar ele no servidor para usarmos o middlewares. Tambem fizemos a configuração no env do REDIS, alem de trocarmos no cache.ts do config para usar as variaveis do env

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -10,11 +10,12 @@ import '@shared/infra/typeorm';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import '@shared/container';
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
-
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
