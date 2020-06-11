f3a347625546573d2dfe5ebd311213c1efaadc77
Maycon <maycondasilvamoreira@hotmail.com>
2020-06-11 12:36:41 -0300

    Foi adicionado o class to class no providersController e foi adicionado no listProvidersService na  hora de salvar o cache.

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,5 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 import User from '@modules/users/infra/typeorm/entities/User';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
@@ -28,7 +29,10 @@ class ListProviderService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
